aedc645a2b8cedcafe11f0b2f3d99c6539c1229c
Kyle Mahan <kyle.mahan@gmail.com>
2016-02-09 07:21:34 -0800

    add missing argument to drawSyndication
    thanks for the heads up @tinokremer!
    
    fixes #2

--- templates/default/entity/IdnoPlugins/Reactions/Like/edit.tpl.php
+++ templates/default/entity/IdnoPlugins/Reactions/Like/edit.tpl.php
@@ -52,7 +52,7 @@ if ($type == 'like') {
                 </div>
             </div>
 
-            <?php if (empty($object->_id)) echo $this->drawSyndication($type); ?>
+            <?php if (empty($object->_id)) echo $this->drawSyndication($type, $object->getPosseLinks()); ?>
 
             <?= $this->draw('content/access'); ?>
             <p class="button-bar" >
