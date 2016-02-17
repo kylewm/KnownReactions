5b9ba5030065796659af95e753219492bad4314c
Kyle Mahan <kyle@kylewm.com>
2016-02-17 00:23:35 +0000

    go back to using Composer; not sure why it stopped working

--- autoloader.php
+++ autoloader.php
@@ -1,5 +1,5 @@
 <?php
-//require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-loader()->registerNamespace('Mf2/Shim', dirname(dirname(__FILE__)) . '/external/mf2/shim');
-loader()->registerNamespace('BarnabyWalters/Mf2', dirname(dirname(__FILE__)) . '/external/barnabywalters/mf-cleaner/src');
+//loader()->registerNamespace('Mf2/Shim', dirname(dirname(__FILE__)) . '/external/mf2/shim');
+//loader()->registerNamespace('BarnabyWalters/Mf2', dirname(dirname(__FILE__)) . '/external/barnabywalters/mf-cleaner/src');
